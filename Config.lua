eab8afc1dfa0e733c7b5c77bd64fa714bea7fb37
TaxD-drop <contafreefiresegura8@gmail.com>
2026-07-27 21:40:20 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -4,6 +4,6 @@ return table.freeze({
     GAME_ID = 3317771874,
     PLACES = table.freeze({
         AUTO_BUY = 119454325063278,
-        FARMING = 140403681187145,
+        FARMING = 140403681187145, 8737899170
     }),
 })
